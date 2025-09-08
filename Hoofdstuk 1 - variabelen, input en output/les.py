435ac07446685dea00d04fcd9e09ee9588508920
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-09-08 12:04:23 +0200

    les1

--- Hoofdstuk 1 - variabelen, input en output/les.py
+++ Hoofdstuk 1 - variabelen, input en output/les.py
@@ -1,3 +1,24 @@
-a = input("Geef een getal: ")
+graag_programmeren = True
+stiften_werken = False
 
-b = a - 2
+job_papa = "Programmeur"
+hobby = 'Klimmen'
+
+dag = 8
+aantal_kinderen = 3
+temperatuur = -2
+
+pi = 3.14159
+begin_school = 8.5
+hoogte = 1.68
+gewicht = 80.0
+
+leerling = "Mathijs"
+Leerling = "Noor"
+
+
+
+begin_school = "Snake case"
+camelCase = "camel case"
+PascalCase = "Pascal case"
+
