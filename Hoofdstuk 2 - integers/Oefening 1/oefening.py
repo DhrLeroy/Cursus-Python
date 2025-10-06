a89bf2c3152b00365eb05d2ae7069eebacd01ac8
DhrLeroy <leroy.renaud@campusimpuls.be>
2025-10-06 13:34:52 +0200

    wisk

--- Hoofdstuk 2 - integers/Oefening 1/oefening.py
+++ Hoofdstuk 2 - integers/Oefening 1/oefening.py
@@ -1,9 +1,2 @@
 a = float(input("Schrijf een getal hier."))
 b = float(input("Schrijf een getal hier."))
-
-som = a + b
-verschil = a - b
-product = a * b
-quotiënt = a / b
-macht = a ** b
-wortel = a ** (1/b)
