514e188fb51ba6a69dd1da0cc26d83c4ff3bed53
Renaud Leroy <remastered.king@gmail.com>
2025-11-21 14:05:15 +0100

    extra oefeningen

--- Hoofdstuk 3 - list en array/les.py
+++ Hoofdstuk 3 - list en array/les.py
@@ -1,4 +1,4 @@
-getallen = []
+#getallen = []
 
 '''getallen.append(int(input("Geef een getal: ")))
 getallen.append(int(input("Geef een getal: ")))
@@ -11,11 +11,92 @@ getallen.append(int(input("Geef een getal: ")))'''
 '''for getal in range(1, 11, 3):
     getallen.append(getal)'''
 
-for i in range(3):
+'''for i in range(3):
     getallen.append(int(input("Geef een getal: ")))
 
 som = 0
 for getal in getallen:
     som = som + getal
 
-print(som)
+print(som)'''
+
+'''getallen.append(1)
+getallen.append(2)
+getallen.append(3)
+getallen.append(4)
+print(getallen[-3:])'''
+
+
+'''letters = []
+
+letters.append("B")
+letters.append("C")
+letters.append("D")
+
+letters.insert(0,"A")
+letters.insert(2,"X")
+letters.insert(10,"Y")
+
+print(letters)
+
+for positie in range(len(letters)):
+    letters[positie] = f'{letters[positie]}.'
+
+for positie in range(5, -1, -1):
+    print(letters[positie])'''
+
+
+
+
+
+
+
+'''letters = []
+
+letters.append("B")
+letters.append("C")
+letters.append("D")
+letters.insert(0,"A")
+letters.insert(1, "X")
+
+for getal in range(0):
+    letter = input("Geef een letter in: ")
+    letters.append(letter)
+
+
+
+
+for plaats_getal in range(len(letters)):
+    letters[plaats_getal] = f'{letters[plaats_getal]}.'
+
+
+
+print(letters)
+'''
+
+
+
+
+
+letters = []
+
+letters.append("b")
+letters.append("c")
+letters.append("d")
+
+letters.insert(0,"a")
+letters.insert(2,"x")
+
+letters.append("z")
+
+for positie in range(len(letters)):
+    letters[positie] = f'{letters[positie]}.'
+
+for letter in letters:
+    print(f'Letter: {letter}')
+print(letters)
+
+
+
+
+
