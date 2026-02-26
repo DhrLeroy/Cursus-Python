8411b332dba151c30ac784c04b82317bd7a6f2dd
RenaudLeroy <RenaudLeroy@WINDOWS-IMF6MMQ>
2026-02-26 12:45:03 +0100

    oefening 10

--- Hoofdstuk 4 - controlestructuren/1. If/oefening10.py
+++ Hoofdstuk 4 - controlestructuren/1. If/oefening10.py
@@ -1,1 +1,16 @@
+leeftijd = int(input("Leeftijd: "))
 
+maximale_hartslag = 208 - (leeftijd*0.7)
+
+hartslag = int(input("Hartslag: "))
+
+percentage = (hartslag / maximale_hartslag) * 100
+
+if percentage < 60:
+    print("Lichte inspanning")
+
+if 60 <= percentage <= 80:
+    print("Matige inspanning")
+
+if percentage > 80:
+    print("Zware inspanning")
